e28ba6effc22295d289dadd0e67d7fb678d15b1a
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-03-23 17:17:47 +0700

    Created Routing

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello Flask Framework</h1>"
 
+@app.route('/about')
+def about():
+    return "<h1>About us</h1>"
+
+@app.route("/admin")
+def admin():
+    return "<h1>Hello Admin</h1>"
+
 if __name__ == "__main__":
     app.run()
